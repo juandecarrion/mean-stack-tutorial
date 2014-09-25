5e77307dc78d12f886e09b4e552b8c8a97c959b8
Dicek <juandeee@gmail.com>
2014-09-25 09:02:37 +0000

    Reformated code, now we are using 2 spaces for tabs

--- app.js
+++ app.js
@@ -2,16 +2,20 @@
 
 
 angular.module('flapperNews', [])
-    .controller('MainCtrl', [
-                 '$scope',
-        function ($scope) {
-            $scope.test = 'Hellow world!';
-            $scope.posts = [                
-              {title: 'post 1', upvotes: 5},
-              {title: 'post 2', upvotes: 2},
-              {title: 'post 3', upvotes: 15},
-              {title: 'post 4', upvotes: 9},
-              {title: 'post 5', upvotes: 4}
-            ];
-        }
-    ]);
+  .controller('MainCtrl', [
+    '$scope',
+    function ($scope) {
+      $scope.test = 'Hellow world!';
+      $scope.posts = [                
+        {title: 'post 1', upvotes: 5},
+        {title: 'post 2', upvotes: 2},
+        {title: 'post 3', upvotes: 15},
+        {title: 'post 4', upvotes: 9},
+        {title: 'post 5', upvotes: 4}
+      ];
+      $scope.addPost = function () {
+        $scope.posts.push({title: $scope.title, upvotes: 0});
+        $scope.title = '';
+      }
+    }
+  ]);
