406ace619424686f1bbf5a499049703f88355557
Dicek <juandeee@gmail.com>
2014-09-25 08:52:10 +0000

    Adding additional information

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ angular.module('flapperNews', [])
                  '$scope',
         function ($scope) {
             $scope.test = 'Hellow world!';
-            $scope.posts = ['post 1',
-                            'post 2',
-                            'post 3',
-                            'post 4',
-                            'post 5'
-                            ];
+            $scope.posts = [                
+              {title: 'post 1', upvotes: 5},
+              {title: 'post 2', upvotes: 2},
+              {title: 'post 3', upvotes: 15},
+              {title: 'post 4', upvotes: 9},
+              {title: 'post 5', upvotes: 4}
+            ];
         }
     ]);
