9b06b828146749f80d46a49472c7f6c78351e52c
Dicek <juandeee@gmail.com>
2014-09-25 12:38:13 +0000

    Added an add comment form

--- js/controllers/postController.js
+++ js/controllers/postController.js
@@ -4,5 +4,17 @@ app.controller('PostCtrl', [
   'postService',
   function($scope, $stateParams, postService){
     $scope.post = postService.posts[$stateParams.id];
+    
+    $scope.addComment = function(){
+      if(!$scope.body) {
+        return;
+      }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
   }
 ]);
