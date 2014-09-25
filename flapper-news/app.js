fbde34eef6450a4b9bd060fe8eefb0d76be6da72
Dicek <juandeee@gmail.com>
2014-09-25 14:12:24 +0000

    Added Express routes to add new Posts and to retrieve them all

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -6,6 +6,12 @@ var cookieParser  = require('cookie-parser');
 var bodyParser    = require('body-parser');
 var mongoose      = require('mongoose');
 
+// Conect to MongoDB
+mongoose.connect('mongodb://localhost/news');
+
+require('./models/Post');
+require('./models/Comment');
+
 var routes  = require('./routes/index');
 var users   = require('./routes/users');
 
@@ -56,11 +62,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
-// Conect to MongoDB
-mongoose.connect('mongodb://localhost/news');
-
-require('./models/Post');
-require('./models/Comment');
 
 module.exports = app;
--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -6,4 +6,30 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if (err){
+      return next(err);
+    }
+
+    res.json(posts);
+  });
+});
+
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+
+  post.save(function(err, post){
+    if(err){
+      return next(err);
+    }
+
+    res.json(post);
+  });
+});
+
 module.exports = router;
