1c9339448eb3d961ed2e8caed3738a70ed07ce5a
Dicek <juandeee@gmail.com>
2014-09-25 08:49:57 +0000

    Getting started

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+
+angular.module('flapperNews', [])
+    .controller('MainCtrl', [
+                 '$scope',
+        function ($scope) {
+            $scope.test = 'Hellow world!';
+            $scope.posts = ['post 1',
+                            'post 2',
+                            'post 3',
+                            'post 4',
+                            'post 5'
+                            ];
+        }
+    ]);
