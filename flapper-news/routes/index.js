c605e1db199cb0f6d8eaefe6988a6c1ea38fbfa1
Dicek <juandeee@gmail.com>
2014-09-25 14:18:15 +0000

    Added Express routes to retrieve particular posts

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -32,4 +32,20 @@ router.post('/posts', function(req, res, next) {
   });
 });
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error("can't find post")); }
+
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
+});
+
 module.exports = router;
